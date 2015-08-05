2683ee20944d92020cd1bdda7b665257f4996532
Ryan Smith <0ryansmith1994@gmail.com>
2015-08-05 11:47:11 +0100

    Adds support for \core\event\user_created

---
+++ tests/UserRegisteredTest.php
@@ -0,0 +1,27 @@
+<?php namespace Tests;
+use \XREmitter\Events\UserRegistered as Event;
+
+class UserRegisteredTest extends EventTest {
+    protected static $recipe_name = 'user_registered';
+
+    /**
+     * Sets up the tests.
+     * @override EventTest
+     */
+    public function setup() {
+        $this->event = new Event($this->repo);
+    }
+
+    protected function constructInput() {
+        return array_merge(
+            parent::constructInput(),
+            $this->contructObject('app')
+        );
+    }
+
+    protected function assertOutput($input, $output) {
+        parent::assertOutput($input, $output);
+        $this->assertVerb('http://adlnet.gov/expapi/verbs/registered', 'registered to', $output['verb']);
+        $this->assertObject('app', $input, $output['object']);
+    }
+}
