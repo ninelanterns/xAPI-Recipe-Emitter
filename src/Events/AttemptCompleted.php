45137a8c9b645e0b607a07d393123e76b965d43c
Andrew Downes <mrdownes@hotmail.com>
2016-01-12 14:51:12 +0000

    Add score and success to attempt completed statement

--- src/Events/AttemptCompleted.php
+++ src/Events/AttemptCompleted.php
@@ -12,18 +12,27 @@ class AttemptCompleted extends AttemptStarted {
      * @override Event
      */
     public function read(array $opts) {
-        return array_merge(parent::read($opts), [
+        $statement = [
             'verb' => [
                 'id' => 'http://adlnet.gov/expapi/verbs/completed',
                 'display' => $this->readVerbDisplay($opts),
             ],
             'result' => [
                 'score' => [
-                    'raw' => $opts['attempt_result'],
+                    'raw' => $opts['attempt_score_raw'],
+                    'min' => $opts['attempt_score_min'],
+                    'max' => $opts['attempt_score_max'],
+                    'scaled' => $opts['attempt_score_scaled']
                 ],
                 'completion' => $opts['attempt_completed'],
                 'duration' => $opts['attempt_duration'],
             ],
-        ]);
+        ];
+
+        if (!is_null($opts['attempt_success'])) {
+            $statement['result']['success'] = $opts['attempt_success'];
+        }
+
+        return array_merge_recursive(parent::read($opts), $statement);
     }
 }
