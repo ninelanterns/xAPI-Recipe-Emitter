8c2195d8dd7dbec68ab9bbd39ea642d1e7f188d0
Andrew Downes <mrdownes@hotmail.com>
2016-01-13 12:12:49 +0000

    student received grade, not tutor graded student
    fixes #24

--- tests/AssignmentGradedTest.php
+++ tests/AssignmentGradedTest.php
@@ -24,11 +24,11 @@ class AssignmentGradedTest extends EventTest {
 
     protected function assertOutput($input, $output) {
         parent::assertOutput($input, $output);
-        $this->assertVerb('http://www.tincanapi.co.uk/verbs/evaluated', 'evaluated', $output['verb']);
-        $this->assertObject('module', $input, $output['context']['contextActivities']['grouping'][2]);
-        $this->assertObject('course', $input, $output['context']['contextActivities']['grouping'][1]);
+        $this->assertVerb('http://adlnet.gov/expapi/verbs/scored', 'recieved grade for', $output['verb']);
+        $this->assertObject('module', $input, $output['object']);
+        $this->assertObject('course', $input, $output['parent'][0]);
         $this->assertEquals($input['grade_result'], $output['result']['score']['raw']);
         $this->assertEquals(true, $output['result']['completion']);
-        $this->assertUser($input, $output['object'], 'graded_user');
+        $this->assertUser($input, $output['actor'], 'graded_user');
     }
 }
