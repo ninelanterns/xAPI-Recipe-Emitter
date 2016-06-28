3274c47867a110360e877e3aae66268735e291d6
Andrew Downes <mrdownes@hotmail.com>
2016-06-28 19:29:08 +0100

    attended docs and tests

---
+++ tests/AttendedTest.php
@@ -0,0 +1,41 @@
+<?php namespace XREmitter\Tests;
+use \XREmitter\Events\Attended as Event;
+
+class AttendedTest extends EventTest {
+    protected static $recipe_name = 'module_viewed';
+
+    /**
+     * Sets up the tests.
+     * @override EventTest
+     */
+    public function setup() {
+        $this->event = new Event($this->repo);
+    }
+
+    protected function constructInput() {
+        return array_merge(
+            parent::constructInput(),
+            $this->contructObject('course'),
+            $this->contructObject('module'),
+            $this->contructObject('session', 'http://activitystrea.ms/schema/1.0/event'),
+            $this->constructUser('attendee'),
+            [
+                "attempt_duration": "PT150S",
+                "attempt_completion": true
+            ]
+        );
+    }
+
+    protected function assertOutput($input, $output) {
+        parent::assertOutput($input, $output);
+        $this->assertUser($input, $output['actor'], 'attendee');
+        $this->assertUser($input, $output['context']['instructor'], 'user');
+        $this->assertVerb('http://adlnet.gov/expapi/verbs/attended', 'attended', $output['verb']);
+        $this->assertObject('question', $input, $output['object']);
+        $this->assertObject('module', $input, $output['context']['contextActivities']['parent'][0]);
+        $this->assertObject('course', $input, $output['context']['contextActivities']['grouping'][1]);
+        $this->assertEquals($input['attempt_duration'], $output['result']['duration']);
+        $this->assertEquals($input['attempt_completion'], $output['result']['completion']);
+        $this->assertEquals('http://xapi.trainingevidencesystems.com/recipes/attendance/0_0_1#simple', $output['context']['contextActivities']['category'][0]['id']);
+    }
+}
